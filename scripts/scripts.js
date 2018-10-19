b45bc10f14710f81c63561ab0acf205107d2be85
leilani <leilanileach@yahoo.com>
2018-10-19 11:31:43 -0700

    add first radio button question and first branch

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // var end = $("input#end").val()
+
+  if (end === "frontend") {
+    $("#CSSTrack").show();
+  } else {
+    $("#secondQuestion").show();
+  }
+});
